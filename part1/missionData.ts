388ff57f410568eac3c43be25d5f673976697b9a
BuildTools <unconfigured@null.spigotmc.org>
2021-03-08 15:50:37 +0100

    ADD: ex01

---
+++ part1/missionData.ts
@@ -0,0 +1,7 @@
+let operationcode = 42;
+let operationAllowed = true;
+let operationName = "KSW";
+let agent = {name: "kreog", code: 42};
+let newbi = {name: "Mate", code: 21};
+let _007 = {code: 21};
+let agents = {agent, newbi, _007};
